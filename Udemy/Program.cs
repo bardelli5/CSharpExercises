b85565d75f8d911be20b535e82c2d2e0c0c9c1b3
x377892 <x377892@PRODAMLBC115624>
2022-10-06 14:09:13 -0300

    Adicionar arquivos de projeto.

---
+++ Udemy/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using System.Net.Mail;
+
+namespace Udemy {
+    internal class Program {
+        static void Main(string[] args) {
+
+            int x = int.Parse(Console.ReadLine());
+
+            int sumAlcool = 0;
+            int sumGasolina = 0;
+            int sumDiesel = 0;
+
+
+            while (x != 4) {
+                if (x > 4) {
+                    x = int.Parse(Console.ReadLine());
+                }
+                if (x == 1) {
+                    sumAlcool++;
+                }
+                else if (x == 2) {
+                    sumGasolina++;
+                }
+                else if (x == 3) {
+                    sumDiesel++;
+                }
+                x = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("MUITO OBRIGADO!");
+            Console.WriteLine("Alcool: " + sumAlcool);
+            Console.WriteLine("Gasolina: " + sumGasolina);
+            Console.WriteLine("Diesel: " + sumDiesel);
+        }
+    }
+}
