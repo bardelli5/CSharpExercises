b90c13b95b4b8390fb2fdec748b045bdaa699e25
Lucas Bardelli de Lima <lucas.bardelli@hotmail.com>
2022-10-13 14:59:09 -0300

    For Exercise

---
+++ Udemy/ForExercise4.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using System.Net.Mail;
+
+/*namespace Udemy {
+    internal class ForExercises {
+        static void Main(string[] args) {
+
+            int n = int.Parse(Console.ReadLine());
+            int fat = 1;
+
+            for (int i = 1; i <= n; i++) {
+                fat = fat * i;
+            }
+            Console.WriteLine(fat);
+        }
+    }
+}*/
