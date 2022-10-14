e398aa5053a83af51b5dd33951ff5d562c0f6657
Lucas Bardelli <lucasbardelli@PRODAM.SP.GOV.BR>
2022-10-14 14:50:28 -0300

    Mais dois exercicios For

---
+++ Udemy/ForExercise5.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Net.Mail;
+
+/*namespace Udemy {
+    internal class ForExercises {
+        static void Main(string[] args) {
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++) {
+                if (n % i == 0) {
+                    Console.WriteLine(i);
+                }
+            }
+            
+        }
+    }
+}*/
---
+++ Udemy/ForExercise6.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+using System.Net.Mail;
+
+namespace Udemy {
+    internal class ForExercises {
+        static void Main(string[] args) {
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++) {
+                Console.WriteLine(i + " " + Math.Pow(i, 2) + " " + Math.Pow(i, 3));
+            }
+            
+        }
+    }
+}
