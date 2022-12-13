bf5ed26e37e995dae283567c38fbf6a9daa9953d
Lucas Bardelli <lucas.bardelli@hotmail.com>
2022-12-13 18:30:42 -0300

    DateTime mais exemplos

--- Udemy/DateTime/ExemplosDateTime.cs
+++ Udemy/DateTime/ExemplosDateTime.cs
@@ -18,7 +18,7 @@ namespace Course {
             Console.WriteLine(d4);
             Console.WriteLine(d5);
             Console.WriteLine(d6);
-            
+
             DateTime d7 = DateTime.Parse("2000-08-15");
             DateTime d8 = DateTime.Parse("2000-08-15 13:05:58");
             DateTime d9 = DateTime.Parse("15/08/2000");
@@ -54,8 +54,39 @@ namespace Course {
             Console.WriteLine(t10);
             Console.WriteLine(t11);
 
+            /*
+             
+            Boa prática
+            • Armazenar em formato UTC (texto: BD / Json / XML)
+            • Instanciar e mostrar em formato Local 
+
+            * Para converter um DateTime para Local ou Utc, você deve usar:
+            • myDate.ToLocalTime()
+            • myDate.ToUniversalTime()  
+            
+             Demo:
+
+                DateTime d1 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Local);
+                DateTime d2 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Utc);
+                DateTime d3 = new DateTime(2000, 8, 15, 13, 5, 58);
+                Console.WriteLine("d1: " + d1);
+                Console.WriteLine("d1 Kind: " + d1.Kind);
+                Console.WriteLine("d1 to Local: " + d1.ToLocalTime());
+                Console.WriteLine("d1 to Utc: " + d1.ToUniversalTime());
+                Console.WriteLine();
+                Console.WriteLine("d2: " + d2);
+                Console.WriteLine("d2 Kind: " + d2.Kind);
+                Console.WriteLine("d2 to Local: " + d2.ToLocalTime());
+                Console.WriteLine("d2 to Utc: " + d2.ToUniversalTime());
+                Console.WriteLine();
+                Console.WriteLine("d3: " + d3);
+                Console.WriteLine("d3 Kind: " + d3.Kind);
+                Console.WriteLine("d3 to Local: " + d3.ToLocalTime());
+                Console.WriteLine("d3 to Utc: " + d3.ToUniversalTime());
+             
+             */
 
 
             }
         }
-}
+    }
